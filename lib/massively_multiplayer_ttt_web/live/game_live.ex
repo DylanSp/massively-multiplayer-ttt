a11426dbbc358e96906704b342c0af22af3cf1d2
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-05-24 19:28:41 -0400

    Wire up basic game logic to view

--- lib/massively_multiplayer_ttt_web/live/game_live.ex
+++ lib/massively_multiplayer_ttt_web/live/game_live.ex
@@ -1,8 +1,43 @@
 defmodule MassivelyMultiplayerTttWeb.GameLive do
   use MassivelyMultiplayerTttWeb, :live_view
+  import MassivelyMultiplayerTtt.Game
 
   def mount(_params, _session, socket) do
-    # socket = assign(socket, key: value)
+    game = %MassivelyMultiplayerTtt.Game{}
+    socket = assign(socket, game: game, status_message: get_status_message(game))
     {:ok, socket}
+  end
+
+  def handle_event("click_cell", %{"cell-num" => cell_num}, socket) do
+    cell_num = String.to_integer(cell_num)
+    {move_result, new_game} = make_move(socket.assigns.game, cell_num)
+
+    socket =
+      case move_result do
+        :game_already_over ->
+          # TODO Replace with flash message
+          IO.puts("game already over")
+          socket
+
+        :square_filled ->
+          # TODO Replace with flash message
+          IO.puts("square filled")
+          socket
+
+        _ ->
+          assign(socket, game: new_game, status_message: get_status_message(new_game))
+      end
+
+    {:noreply, socket}
+  end
+
+  defp get_status_message(game) do
+    case {game.winning_player, game.current_player} do
+      {:player_x, _} -> "Player X has won!"
+      {:player_o, _} -> "Player O has won!"
+      {:drawn, _} -> "Game is drawn!"
+      {_, :player_x} -> "Player X to move"
+      {_, :player_o} -> "Player O to move"
+    end
   end
 end
