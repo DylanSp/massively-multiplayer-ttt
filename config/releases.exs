b3492eac7d6b72549b8e5249547da1c167c444df
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-05-25 14:29:48 -0400

    Correct app name in releases config

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :massively_multiplayer_tic_tac_toe, MassivelyMultiplayerTttWeb.Endpoint,
+config :massively_multiplayer_ttt, MassivelyMultiplayerTttWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
