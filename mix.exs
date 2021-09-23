21f65363aa5314946cc7a4548a91d9df2a1541bc
Dylan Sprague <dylan.richard.sprague@gmail.com>
2021-09-23 09:52:33 -0400

    Configure for fly.io deployment

--- mix.exs
+++ mix.exs
@@ -33,12 +33,12 @@ defmodule MassivelyMultiplayerTtt.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix, "~> 1.5.13"},
+      {:phoenix_live_view, "~> 0.16.4"},
       {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.4"},
+      {:phoenix_live_dashboard, "~> 0.5.2"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
